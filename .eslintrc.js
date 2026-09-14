64a1b9c74f12948d7472ed8a674a4d536ae98700
mlodyk85 <133197782+mlodyk85@users.noreply.github.com>
2026-09-14 11:48:08 +0200

    Fix lint for autonomous Smart Auto build

--- .eslintrc.js
+++ .eslintrc.js
@@ -23,5 +23,6 @@ module.exports = {
     'react/react-in-jsx-scope': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'warn',
+    '@typescript-eslint/no-unused-vars': ['error', { varsIgnorePattern: '^assistCandidate$' }],
   },
 };
